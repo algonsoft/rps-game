6b0b94e4bb0b7047b3335ec91ff6a97e4f05cf4f
algonsoft <smtalgonsoft@gmail.com>
2021-12-16 15:32:51 +1030

    Finished UI for RPS Game

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
-let computerAction, action;
+let computerAction;
+let passer;
 let playerScore = 0;
 let computerScore = 0;
+let flag = 1;
+let remove = false;
+
 
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 3);
-  console.log(randomNumber);
   if (randomNumber == 0) {
     computerAction = "rock";
     return computerAction;
@@ -17,62 +20,136 @@ function computerPlay() {
     computerAction = "scissors";
     return computerAction;
   }
-  
   }
-
-
-
-function humanPlay(){
-  action = prompt("Please enter: Rock, Paper or Scissors");
-  action = action.toLowerCase();
   
-  
-  if(action == "rock" || action == "paper" || action == "scissors"){
-    console.log(action);
-    return action;
 
-  } 
+
+
+
+
+
+
+  function myClickHandler(){
+    const buttons = document.querySelectorAll(`button`);
+ 
+  if( flag==1){
+  
+  buttons.forEach((button) => {
+      button.addEventListener(`click`, () => {
+      action = (button.id);
+      passer = (button.id);
+      console.log(action);
+      console.log(flag);
+      Game();
+      });
+    });
+  }
   else {
-    humanPlay();
+    document.querySelectorAll(`button`).removeEventListener(`click`, myClickHandler);
+
   }
 }
+  
+
+    
+    
+     
+      
+      
+    
+      
+    
+  
+
+
+
+    
+  
+
+
+
+
+
+
+
 
 function rockPaperScissors(action, computerAction){
-  action = humanPlay();
+  const results = document.querySelector('#results');
   computerAction = computerPlay();
+  action = passer;
+  console.log(action);
   if (action == computerAction){
-    alert("You have selected " + action + " and the computer has selected " + computerAction + " it is a draw" + " Computer: " + computerScore + " Player: " + playerScore)
+    const content = document.createElement(`div`)
+    content.textContent = "You have selected " + action + " and the computer has selected " + computerAction + " it is a draw" + " Computer: " + computerScore + " Player: " + playerScore;
+    results.appendChild(content);
+   
   }
   else if (action == "rock" && computerAction == "scissors"){
     playerScore ++;
-    alert("You have selected " + action + " and the computer has selected " + computerAction + " you win!" + " Computer: " + computerScore + " Player: " + playerScore);    
+    const content = document.createElement(`div`)
+    content.textContent = "You have selected " + action + " and the computer has selected " + computerAction + " you win!" + " Computer: " + computerScore + " Player: " + playerScore;
+    results.appendChild(content);
   }
   else if (action == "paper" && computerAction == "rock"){
     playerScore++;
-    alert("You have selected " + action + " and the computer has selected " + computerAction + " you win!" + " Computer: " + computerScore + " Player: " + playerScore);    
+    const content = document.createElement(`div`)
+    content.textContent = "You have selected " + action + " and the computer has selected " + computerAction + " you win!" + " Computer: " + computerScore + " Player: " + playerScore;
+    results.appendChild(content);  
   }
   else if (action == "scissors" && computerAction == "paper"){
     playerScore++;
-    alert("You have selected " + action + " and the computer has selected " + computerAction + " you win!" + " Computer: " + computerScore + " Player: " + playerScore);   
+    const content = document.createElement(`div`)
+    content.textContent = "You have selected " + action + " and the computer has selected " + computerAction + " you win!" + " Computer: " + computerScore + " Player: " + playerScore;  
+    results.appendChild(content);
   }
   else{
     computerScore++;
-    alert("You have selected " + action + " and the computer has selected " + computerAction + " you lose!" + " Computer: " + computerScore + " Player: " + playerScore);  
+    const content = document.createElement(`div`)
+    content.textContent = "You have selected " + action + " and the computer has selected " + computerAction + " you lose!" + " Computer: " + computerScore + " Player: " + playerScore;
+    results.appendChild(content);
   }
 }
 
-function Game(){
+function Game(action, button, Click){
+  action = passer;
+  console.log(action);
   console.log(playerScore, computerScore);
-  while(playerScore < 5 && computerScore < 5){
-    rockPaperScissors();
-  }
+  rockPaperScissors();
+  
   if(playerScore == 5){
-    alert("The player has " + playerScore + " wins to the computers " + computerScore + " the player wins!");
-  }
+    const content = document.createElement(`div`);
+    content.textContent = "The player has " + playerScore + " wins to the computers " + computerScore + " the player wins!";
+    results.appendChild(content);
+    var button = document.getElementById("rock");
+    button.remove();
+    var button = document.getElementById("paper");
+    button.remove();
+    var button = document.getElementById("scissors");
+    button.remove();
+
+    
+
+
+
+
+
+
+}
+
   else if(computerScore == 5){
-    alert("The computer has " + computerScore + " wins to the players " + playerScore + " The computer wins!");
+    const content = document.createElement(`div`)
+    content.textContent = "The computer has " + computerScore + " wins to the players " + playerScore + " The computer wins!";
+    results.appendChild(content);
+    var button = document.getElementById("rock");
+    button.remove();
+    var button = document.getElementById("paper");
+    button.remove();
+    var button = document.getElementById("scissors");
+    button.remove();
+
+    
   }
 
 }
 
-Game();
+myClickHandler();
